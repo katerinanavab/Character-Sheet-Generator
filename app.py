72afa9ac25f0c0a5363d5615ec6b9585d208cb5e
Katerina (Navab) Walter <94501766+katerinanavab@users.noreply.github.com>
2025-01-24 19:12:24 +0000

    templates

--- app.py
+++ app.py
@@ -4,14 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('base.html')  # Or a separate homepage template
+    return render_template('index.html')
 
 @app.route('/class/<class_name>')
 def class_page(class_name):
-    valid_classes = ['barbarian', 'wizard', 'rogue']  # Add all valid classes
-    if class_name not in valid_classes:
-        return "Class not found", 404
     return render_template(f'classes/{class_name}.html', class_name=class_name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5421, debug=True)
