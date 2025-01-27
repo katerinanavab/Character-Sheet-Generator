a316e567d0595e75647e62c77f172933abe891d1
Katerina (Navab) Walter <94501766+katerinanavab@users.noreply.github.com>
2025-01-27 16:22:23 +0000

    added class descriptions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,6 +9,18 @@ def index():
 @app.route('/class/<class_name>')
 def class_page(class_name):
     return render_template(f'classes/{class_name}.html', class_name=class_name)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    # Collect form data
+    user_data = {
+        'name': request.form.get('name'),
+        'age': request.form.get('age'),
+        'favorite_color': request.form.get('favorite_color'),
+        'favorite_hobby': request.form.get('favorite_hobby'),
+        'lucky_number': request.form.get('lucky_number')
+    }
+    return render_template('sheet.html', user_data=user_data)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5421, debug=True)
