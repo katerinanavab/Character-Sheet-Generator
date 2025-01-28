591427dd846ae2d81775d9f52c86b13a60f60275
Katerina (Navab) Walter <94501766+katerinanavab@users.noreply.github.com>
2025-01-28 18:52:43 +0000

    fixed default values

--- app.py
+++ app.py
@@ -13,12 +13,16 @@ def class_page(class_name):
 @app.route('/submit', methods=['POST'])
 def submit():
     # Collect form data
+    name = request.form.get('name')
+    if not name:
+        name = 'Tav'
     user_data = {
-        'name': request.form.get('name', 'Tav'),
+        'name': name,
         'race': request.form.get('race'),
         'background': request.form.get('background')
     }
+    print(f"Received form data: {user_data}")
     return render_template('sheet.html', user_data=user_data)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5421, debug=True)
+    app.run(host="0.0.0.0", port=9999, debug=True)
